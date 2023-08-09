7e162838756a91d466877365a1f90a6f4c2aacd9
renuka-pq <satyarenuka04@gmail.com>
2023-08-09 12:23:44 +0530

    commited

---
+++ project1/src/com/klkl/Sandhya.java
@@ -0,0 +1,18 @@
+package com.klkl;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sandhya {
+	
+	
+	  @Test
+	  public void f() 
+	  {
+		  WebDriver driver = new ChromeDriver ();
+		  driver.get("https://www.alpha-airtable.com/enterprise");
+	  }
+	}
+
+
---
+++ project1/src/project1/NewTest.java
@@ -0,0 +1,9 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
